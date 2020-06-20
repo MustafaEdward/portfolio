ce7864b94144550953a6ab6341050929fe59bbf5
Mustafa <mustafa.edward24@gmail.com>
2020-06-20 00:05:26 -0400

    new update

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -6,26 +6,3 @@ $('.carousel').flickity({
   pageDots: false,
 });
 
-
-$(document).ready(function(){
-  TweenMax.set(".project-preview", {width:0});
-
-  var t1= new TimeLineLite();
-
-  $(document).on("mouseover", ".number__container", function(evt) {
-    t1 = new TimeLineLite();
-    t1.to($(".project-preview"), 1, {
-      width: "600px",
-      ease: Exp.easeInOut 
-
-    }).on("mouseout", ".navigation-item", function(evt) {
-      t1= new TimeLineLite();
-      t1.to($(".project-preview"), 0.5, {
-        width: 0,
-        ease: Exp.easeInOut 
-  
-      });
-    })
-
-  });
-})
