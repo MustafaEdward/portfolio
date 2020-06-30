eb4a33eb1be0a5502892b06acf350de6b5903d1c
Mustafa <mustafa.edward24@gmail.com>
2020-06-30 18:32:11 -0400

    scroll

---
+++ javascript/animation.js
@@ -0,0 +1,53 @@
+
+
+
+//onload animi.js
+// document.addEventListener('DOMContentLoaded', () => {
+//     //initial
+//     anime.timeline({
+//         easing: 'easeOutExpo',
+//         // duration: 750
+//       })
+//     .add({
+//         targets: '.welcome',
+//         easing: 'easeInOutQuad',
+//         delay: 1000,
+//         height:['100vh', '0'],
+//         direction: "alternate-reverse",
+//         duration: 1000,
+//       })
+   
+   
+// })
+
+
+//onscroll
+// $(window).scroll(function() {
+//     var hT = $('#scroll-to').offset().top,
+//         hH = $('#scroll-to').outerHeight(),
+//         wH = $(window).height(),
+//         wS = $(this).scrollTop();
+//      console.log((hT-wH) , wS);
+//     if (wS > (hT+hH-wH)){
+        
+//     console.log("red")
+//     anime.timeline({
+//                 easing: 'easeOutExpo',
+//                 // duration: 750
+//               })
+//             .add({
+//                 targets: '.more-text',
+//                 easing: 'easeInOutQuad',
+//                 translatex: [0, 2000],
+//                 duration: 4000,
+//                 delay: 500                
+//               })
+//     }
+//  });
+
+
+
+
+$(document).on('scroll', function(){
+    $('.more-text').css("left", Math.max(900 - 0.35*window.scrollY) + "px")
+})
