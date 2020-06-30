d407902fec00c4e1b2c4ce242aec2f9e91a1c832
Mustafa <mustafa.edward24@gmail.com>
2020-06-30 14:08:50 -0400

    image distortio

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: '../img/profile.png',
+    image2: '../img/profile.png',
+    displacementImage: '../img/heightMap.png'
+})
