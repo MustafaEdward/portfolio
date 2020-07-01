d365fcf1aa55f4d5de5b7872cb65297f82115d10
Mustafa <mustafa.edward24@gmail.com>
2020-06-30 20:03:52 -0400

    images fluid

--- javascript/animation.js
+++ javascript/animation.js
@@ -48,6 +48,16 @@
 
 
 
+//text movement on scroll
+
 $(document).on('scroll', function(){
     $('.more-text').css("left", Math.max(900 - 0.35*window.scrollY) + "px")
+})
+
+$(document).on('scroll', function(){
+    $('.more-text-projects').css("left", Math.max(2150 - 0.35*window.scrollY) + "px")
+})
+
+$(document).on('scroll', function(){
+    $('.more-text-interactions').css("left", Math.max(3000 - 0.35*window.scrollY) + "px")
 })
