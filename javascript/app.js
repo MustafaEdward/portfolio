ab9beb08854ca15f9eaab8933e43904888512979
Mustafa <mustafa.edward24@gmail.com>
2020-07-10 14:19:10 -0400

    video

--- javascript/app.js
+++ javascript/app.js
@@ -8,4 +8,42 @@ new hoverEffect({
 
 
 
+//onload 
+
+// const body = document.querySelector('body');
+const cover = document.querySelector('.start')
+
+// const fade = () => {
+//     setTimeout(()=> {
+//         body.classList.toggle('fade-out');
+//     }, 4000);
+  
+// }
+
+// body.addEventListener('load', fade());
+
+
+
+
+
+//Smoothscroll vanila JS
+// const body = document.body,
+// scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+//                 height = scrollWrap.getBoundingClientRect().height - 1,
+//                 speed = 0.04;
+
+//             var offset = 0;
+
+//             body.style.height = Math.floor(height) + "px";
+
+//             function smoothScroll() {
+//                 offset += (window.pageYOffset - offset) * speed;
+
+//                 var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//                 scrollWrap.style.transform = scroll;
+
+//                 callScroll = requestAnimationFrame(smoothScroll);
+//             }
+
+//             smoothScroll();
 
