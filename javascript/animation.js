61fb2ce74b2f5b78216f2d6f658fb867be169a42
Mustafa <mustafa.edward24@gmail.com>
2020-07-18 23:37:37 -0400

    new

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,5 +1,4 @@
 //text movement on scroll
-
 $(document).on('scroll', function(){
     $('.more-text').css("left", Math.max(900 - 0.35*window.scrollY) + "px")
 })
@@ -7,15 +6,4 @@ $(document).on('scroll', function(){
 $(document).on('scroll', function(){
     $('.more-text-projects').css("left", Math.max(3000 - 0.35*window.scrollY) + "px")
 })
-
-
-//smaller screens
-$(document).on('scroll', function(){
-    $('.more-text-port').css("left", Math.max(500 - 0.35*window.scrollY) + "px")
-})
-
-$(document).on('scroll', function(){
-    $('.more-text-projects-port').css("left", Math.max(1500 - 0.35*window.scrollY) + "px")
-})
-
 
