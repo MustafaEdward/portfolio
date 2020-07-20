707f375095277fc93e79fd604ca2fa73101f9b04
Mustafa <mustafa.edward24@gmail.com>
2020-07-20 15:52:03 -0400

    done

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,10 +1,10 @@
 //text movement on scroll
 $(document).on('scroll', function(){
-    $('.more-text').css("left", Math.max(900 - 0.35*window.scrollY) + "px")
+    $('.more-text').css("left", Math.max(800 - 0.35*window.scrollY) + "px")
 })
 
 $(document).on('scroll', function(){
-    $('.more-text-projects').css("left", Math.max(3000 - 0.35*window.scrollY) + "px")
+    $('.more-text-projects').css("left", Math.max(3100 - 0.35*window.scrollY) + "px")
 })
 
 
